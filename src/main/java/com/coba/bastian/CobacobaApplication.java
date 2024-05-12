9cb9280fb104b1e2214e28a52629d9bf11cec8d5
NairB <brian.koeswanto@infovesta.com>
2024-05-12 09:48:07 +0700

    feat : first commit

---
+++ src/main/java/com/coba/bastian/CobacobaApplication.java
@@ -0,0 +1,13 @@
+package com.coba.bastian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobacobaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobacobaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coba/bastian/CobacobaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coba.bastian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobacobaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
