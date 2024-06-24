f58c0ce357df362d50c243bb42614915ef73f914
bastian <bastiansetyo837@gmail.com>
2024-06-24 18:14:57 +0700

    feat : add study palindrom

---
+++ src/main/java/com/coba/bastian/controller/palindrome.java
@@ -0,0 +1,45 @@
+package com.coba.bastian.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Scanner;
+
+@Slf4j
+@RestController
+
+public class palindrome {
+
+    @PostMapping("/palindrom")
+    public String palindrom(@RequestBody String inputan){
+        String cekpali = "";
+        int input;
+        System.out.print("masukan teks yang di cek : ");
+        input = inputan.length();
+        for (int i = input - 1; i >= 0; i--)
+            cekpali = cekpali + inputan.charAt(i);
+        if (inputan.equals(cekpali))
+            return "palindrom";
+        else
+            return "tidak palindrom";
+    }
+
+   /* public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("masukan teks yang di cek : ");
+        String inputan = scanner.nextLine();
+        String cekpali = "";
+        int input;
+
+        input = inputan.length();
+        for (int i = input - 1; i >= 0; i--)
+            cekpali = cekpali + inputan.charAt(i);
+        if (inputan.equals(cekpali))
+            System.out.print("palindrom");
+        else
+            System.out.print("tidak palindrom");
+    }*/
+
+}
