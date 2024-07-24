443839bab7c3dd242a2dbab39dc0a87cb08e2d0e
NairB <brian.koeswanto@infovesta.com>
2024-07-24 12:44:52 +0700

    Style : add config db generator

--- src/main/java/com/coba/bastian/config/JPAGeneratorConfig.java
+++ src/main/java/com/coba/bastian/config/JPAGeneratorConfig.java
@@ -1,62 +1,58 @@
-//package com.coba.bastian.config;
-//
-//
-//import com.zaxxer.hikari.HikariConfig;
-//import com.zaxxer.hikari.HikariDataSource;
-//import jakarta.persistence.EntityManagerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.boot.jdbc.DataSourceBuilder;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.context.annotation.Primary;
-//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-//import org.springframework.orm.jpa.JpaTransactionManager;
-//import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-//import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-//import org.springframework.transaction.PlatformTransactionManager;
-//
-//import javax.sql.DataSource;
-//import java.util.Objects;
-//
-//@Configuration
-//@ConfigurationProperties("spring.datasource.secondary")
-//@EnableJpaRepositories(
-//        basePackages = {"com.coba.bastian.data"},
-//        entityManagerFactoryRef = "secondaryEntityManagerFactory",
-//        transactionManagerRef = "secondaryTransactionManager"
-//)
-//public class JPAGeneratorConfig extends HikariConfig{
-//
-////    @Autowired
-//    private HibernateConfig hibernateConfig;
-//
-//    @Bean(name = "SecondaryDataSource")
-//    public HikariDataSource generatorManagementDataSource() {
-//
-//     return new HikariDataSource(this);
-//    }
-//
-//    @Bean(name = "secondaryEntityManagerFactory")
-//    public LocalContainerEntityManagerFactoryBean secondaryEntityManagerFactory() {
-//        HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
-//        jpaVendorAdapter.setGenerateDdl(false);
-//
-//        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
-//
-//        factoryBean.setDataSource(generatorManagementDataSource());
-//        factoryBean.setJpaVendorAdapter(jpaVendorAdapter);
-//        factoryBean.setPackagesToScan("com.coba.bastian.domain");
-//        factoryBean.setPersistenceUnitName("secondarypersistenceunit");
-//        factoryBean.setJpaProperties(hibernateConfig.toProperties());
-//        return factoryBean;
-//    }
-//
-//    @Bean
-//    public PlatformTransactionManager secondaryTransactionManager() {
-//        return new JpaTransactionManager(Objects.requireNonNull(secondaryEntityManagerFactory().getObject()));
-//    }
-//
-//
-//}
+package com.coba.bastian.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.Objects;
+
+@Configuration
+@ConfigurationProperties("spring.datasource.generator.hikari")
+@EnableJpaRepositories(
+        basePackages = {"com.coba.bastian.repository"},
+        entityManagerFactoryRef = "generatorManagementEntityManagerFactory",
+        transactionManagerRef = "generatorManagementTransactionManager"
+)
+public class JPAGeneratorConfig extends HikariConfig {
+
+    @Autowired
+    private HibernateConfig hibernateConfig;
+
+    @Primary
+    @Bean
+    public HikariDataSource generatorManagementDataSource() {
+        return new HikariDataSource(this);
+    }
+
+    @Primary
+    @Bean("generatorManagementEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean generatorManagementEntityManagerFactory() {
+            HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+        jpaVendorAdapter.setGenerateDdl(false);
+
+    LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+
+        HikariDataSource hd = generatorManagementDataSource();
+
+        factoryBean.setDataSource(hd);
+        factoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+        factoryBean.setPackagesToScan("com.coba.bastian.dao");
+        factoryBean.setPersistenceUnitName("generatorpersistenceunit");
+        factoryBean.setJpaProperties(hibernateConfig.toProperties());
+        return factoryBean;
+    }
+
+    @Bean
+    public PlatformTransactionManager generatorManagementTransactionManager() {
+        return new JpaTransactionManager(Objects.requireNonNull(generatorManagementEntityManagerFactory().getObject()));
+    }
+}
