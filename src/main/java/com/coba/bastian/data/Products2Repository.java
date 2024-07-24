612addb4219593ade52d3b70f47cd023eae6b60d
NairB <brian.koeswanto@infovesta.com>
2024-07-24 12:41:22 +0700

    Style : add repository db ipa

--- src/main/java/com/coba/bastian/data/Products2Repository.java
+++ src/main/java/com/coba/bastian/data/Products2Repository.java
@@ -1,8 +1,8 @@
 package com.coba.bastian.data;
 
-import com.coba.bastian.dao.Products2Dao;
+import com.coba.bastian.dao.ProductsIpaDao;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface Products2Repository extends JpaRepository<Products2Dao, String> {
+public interface Products2Repository extends JpaRepository<ProductsIpaDao, String> {
 
 }
