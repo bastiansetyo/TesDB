a4cb7221928bf6ed4431cfb9df306e275686b0bd
NairB <brian.koeswanto@infovesta.com>
2024-07-24 12:45:38 +0700

    Style : add test app

--- src/main/java/com/coba/bastian/CobacobaApplication.java
+++ src/main/java/com/coba/bastian/CobacobaApplication.java
@@ -3,15 +3,22 @@ package com.coba.bastian;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication(
 scanBasePackages = {
-		"com.coba.bastian.data",
-		"com.coba.bastian.repository"
+//		"com.coba.bastian.data",
+		"com.coba.bastian"
 		})
 @EnableTransactionManagement
-public class CobacobaApplication {
+public class CobacobaApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CobacobaApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CobacobaApplication.class, args);
 	}
--- src/test/java/com/coba/bastian/CobacobaApplicationTests.java
+++ src/test/java/com/coba/bastian/CobacobaApplicationTests.java
@@ -1,13 +1,27 @@
 package com.coba.bastian;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@SpringBootTest
-class CobacobaApplicationTests {
+@SpringBootApplication(
+		scanBasePackages = {
+				"com.coba.bastian"
+		})
+@EnableTransactionManagement
+public class CobacobaApplicationTests extends SpringBootServletInitializer {
 
-	@Test
-	void contextLoads() {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CobacobaApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobacobaApplication.class, args);
 	}
 
 }
